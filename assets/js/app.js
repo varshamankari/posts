36c1e1ed6e72740c8219097b7612601068443bf3
varshamankari <106649456+varshamankari@users.noreply.github.com>
2022-08-04 14:36:57 +0530

    post
    my first card

---
+++ assets/js/app.js
@@ -0,0 +1,72 @@
+let cl=console.log;
+
+let data = [
+    {
+        "albumId": 1,
+        "id": 1,
+        "title": "accusamus beatae ad facilis cum similique qui sunt",
+        "url": "https://images.unsplash.com/photo-1659485261351-5829b87386fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+    },
+    {
+        "albumId": 1,
+        "id": 2,
+        "title": "reprehenderit est deserunt velit ipsam",
+        "url": "https://images.unsplash.com/photo-1659175183211-de155980fa7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        "thumbnailUrl": "https://via.placeholder.com/150/771796"
+    },
+    {
+        "albumId": 1,
+        "id": 3,
+        "title": "officia porro iure quia iusto qui ipsa ut modi",
+        "url": "https://images.unsplash.com/photo-1659163362440-6c211f05b05b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        "thumbnailUrl": "https://via.placeholder.com/150/24f355"
+    },
+    {
+        "albumId": 1,
+        "id": 4,
+        "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+        "url": "https://images.unsplash.com/photo-1658844072437-bc884abe8069?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80",
+        "thumbnailUrl": "https://via.placeholder.com/150/d32776"
+    },
+    {
+        "albumId": 1,
+        "id": 5,
+        "title": "natus nisi omnis corporis facere molestiae rerum in",
+        "url": "https://images.unsplash.com/photo-1659221483765-1678962b7c9a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80",
+        "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
+    },
+    {
+        "albumId": 1,
+        "id": 6,
+        "title": "accusamus ea aliquid et amet sequi nemo",
+        "url": "https://images.unsplash.com/photo-1658847412035-2ae0d3d5c364?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        "thumbnailUrl": "https://via.placeholder.com/150/56a8c2"
+    }]
+
+    let postInfo = document.getElementById("postInfo");
+
+    let result=``
+    data.forEach((post) =>{
+        result += `
+        <div class="col-md-4 mb-4">
+        <div class="card">
+           <div class="card-body">
+            <figure class="post">
+                <img 
+                src="${post.url}" class="img-fluid" alt="">
+                <figcaption>
+                    <p>
+                        ${post.title}
+                    </p>
+                </figcaption>
+                <span class="id">ID : ${post.id}</span>
+                <span class="albumId">AlbumId : ${post.albumId}</span>
+            </figure>
+           </div>
+        </div>
+    </div>
+        `
+    })
+
+    postInfo.innerHTML=result;
